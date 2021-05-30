b5304fb4f9284d62e55b470224c4a6b566ae2129
Abhishek Jaswal <76031353+AbhishekJaswal2001@users.noreply.github.com>
2021-05-30 12:24:20 +0530

    Add files via upload

--- source/conn.java
+++ source/conn.java
@@ -1,1 +1,18 @@
+package Empolyee;
+import java.sql.*;
 
+public class conn{
+    
+    public Connection c;
+    public Statement s;
+ 
+    public conn(){
+        try{
+        	Class.forName("com.mysql.cj.jdbc.Driver");
+			c=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","KRITIKAKAURA","kritika@1928");
+            s = c.createStatement();
+        }catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
